1e4d56b29db510c1a5ef79071aaa182506967d67
Sara Linde <lindesara@gmail.com>
2014-06-24 20:23:36 -0700

    Initial.

---
+++ btd.py
@@ -0,0 +1,32 @@
+import webbrowser
+
+# This launches my 6 favorite websites in one script!
+
+
+new=2;
+url="https://www.gmail.com";
+webbrowser.open(url, new=new);
+
+new=2;
+url="http://www.metafilter.com/";
+webbrowser.open(url, new=new);
+
+new=2;
+url="http://slog.thestranger.com/blogs/slog/";
+webbrowser.open(url, new=new);
+
+new=2;
+url="http://www.kirotv.com/";
+webbrowser.open(url, new=new);
+
+new=2;
+url="http://www.facebook.com";
+webbrowser.open(url, new=new);
+
+new=2;
+url="http://forums.somethingawful.com/usercp.php";
+webbrowser.open(url, new=new);
+
+
+
+
